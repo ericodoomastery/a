614f02389cdf79a7dd91399e2ec700d7d725a293
ericodoomastery <ericzohoapps@gmail.com>
2026-02-25 11:16:39 +0200

    Update __manifest__.py

--- a_module/__manifest__.py
+++ a_module/__manifest__.py
@@ -1,1 +1,13 @@
-
+# my_module/__manifest__.py
+{
+    "name": "AttachReturn",
+    "version": "1.0",
+    "summary": "Custom model example",
+    "category": "Custom",
+    "author": "Eric",
+    "license": "LGPL-3",
+    "depends": ["base", "stock"],  # add stock for stock moves/products
+    "data": [],
+    "installable": True,
+    "application": True,
+}
